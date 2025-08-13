7ca8f988efe90937205acb40509d8dc47051df1d
dhanush580 <pkveeragautham10@gmail.com>
2025-08-13 15:50:19 +0530

    learn variables

--- getting started with c++/variables.cpp
+++ getting started with c++/variables.cpp
@@ -22,5 +22,11 @@ int main(){
     int num1=20;
     int NUM1=30;
     cout << _num << "\t" << num1 << "\t" << NUM1;
+
+    //we can also declare constants with const keyword, this values cant be changed later in any part of the program
+    const int pie=3.14;
+    cout<<pie;
+    //if we try to redeclare pie= 5.8; we get error: ssignment of read-only variable 'pie'.
     return 0;
+
 }
