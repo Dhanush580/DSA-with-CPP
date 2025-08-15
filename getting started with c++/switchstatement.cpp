5499db4c51ca84c7235c5fdf7e6e49337d6b3a9c
dhanush580 <pkveeragautham10@gmail.com>
2025-08-15 11:27:12 +0530

    learnt switch statement

---
+++ getting started with c++/switchstatement.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int day;
+    cout<<"Tutorial for switch statement"<<endl;
+    cout<<"enter any number of the week"<<endl;
+
+    cin>>day;
+
+    switch(day){
+        case 1:
+        cout<<"Sunday";
+        break;
+        case 2:
+        cout<<"Monday";
+        break;
+        case 3:
+        cout<<"tuesday";
+        break;
+        case 4:
+        cout<<"Wednesday";
+        break;
+        case 5:
+        cout<<"Thursday";
+        break;
+        case 6:
+        cout<<"Friday";
+        break;
+        case 7:
+        cout<<"Saturday";
+        break;
+        default:
+        cout<<"choose a proper number"<<endl;
+
+    }
+
+}
