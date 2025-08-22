9beebf89eb6d81ad7b2c994a910e8726d82585e8
dhanush580 <pkveeragautham10@gmail.com>
2025-08-22 20:02:01 +0530

    practiced patterns

---
+++ DSA/patterns/pattern7.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+/********************************************************* PATTERN-5 **************************************************************/
+/*
+                                                              *
+                                                             ***
+                                                            *****
+                                                           *******
+                                                          *********
+*/
+int main(){
+    //static way
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5-i-1;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<(2*i)+1;j++){
+            cout<<"*";
+        }
+        for(int j=0;j<5-i-1;j++){
+            cout<<" ";
+        }
+    cout<<endl;
+    }
+
+
+
+    //dynamix way
+    int n ;
+    cout<<"Enter no.of rows:";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<(2*i)+1;j++){
+            cout<<"*";
+        }
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+    cout<<endl;
+    }
+    return 0;
+}
