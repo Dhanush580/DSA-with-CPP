e991c9034fbe3433223e9634150ba2e2f5d1f2e5
Dhanush Pekala <119430623+Dhanush580@users.noreply.github.com>
2026-03-03 10:12:16 +0530

    Added prime counting program between A and B
    Implement a program to count and print all prime numbers between two given integers.

---
+++ DSA/basic_math/Count primes between A and B.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isprime(int n){
+    int count=0;
+    for(int i=1;i*i<=n;i++){
+        if(n%i==0){
+            count++;
+            if(n/i!=i){
+                count++;
+            }
+        }
+    }
+    if(count==2) return true;
+    else return false;
+}
+
+int main() {
+	//print all prime numbers in the given range
+	int a;
+	int b;
+	cin>>a>>b;
+	for(int i=a;i<=b;i++){
+	    if(isprime(i)){
+	        cout<<i<<" ";
+	    }
+	}
+}
